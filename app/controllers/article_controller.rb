27262aa05dffd54981b3b16e8710c270e33e7c8d
Vik Ewoods <support@voroninstudio.eu>
2013-04-26 14:31:22 +0300

    Temp function to add articles
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,7 +1,23 @@
 class ArticleController < ApplicationController
   def index
+    @articles = Article.order('id').page(params[:page]).per_page(10)
   end
 
   def show
+    @article ||= Article.find_by_slug!(params[:id])
+  end
+
+  #Temporary function
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to article_path, notice: "Successfully created product."
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ UniAbroad::Application.routes.draw do
   match 'about-us' => 'page#about', :as => 'about', :via => 'get'
   match 'contact-us' => 'page#contact', :as => 'contact', :via => 'get'
   match 'articles' => 'article#index', :as => 'article', :via => 'get'
+  match 'articles' => 'article#create', :as => 'article_create', :via => 'post'
+  match 'articles/new' => 'article#new', :as => 'article_new', :via => 'get'
   match 'articles/:id' => 'article#show', :as => 'article_show', :via => 'get'
   match 'students' => 'student#index', :as => 'student', :via => 'get'
   match 'students/:id' => 'student#show', :as => 'student_show', :via => 'get'
