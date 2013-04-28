820d96c93b9c5eed16e1e0ebfd0169556e595bce
Vik Ewoods <support@voroninstudio.eu>
2013-04-28 22:12:26 +0300

    Rails admin and kamanari pagination fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 UniAbroad::Application.routes.draw do
+  mount RailsAdmin::Engine => '/manage', :as => 'rails_admin'
+
   devise_for :admins
 
   match 'about-us' => 'page#about', :as => 'about', :via => 'get'
