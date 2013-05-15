51e4f1609bc495849bd01a584edee3cb31fd5838
Vik Ewoods <support@voroninstudio.eu>
2013-05-15 14:07:30 +0300

    Getting 4 random records from DB
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,7 @@
 class PageController < ApplicationController
   def index
-    @last ||= Article.offset(rand(Article.count)).last(4)
+    #@last ||= Article.offset(rand(Article.count)).last(4)
+    @last ||= Article.find(:all, :order => "RANDOM()", :limit => 4)
   end
 
   def about
