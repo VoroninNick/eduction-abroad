8d372995ffe26002fa981ee479b2594d6742a7d8
Vik Ewoods <support@voroninstudio.eu>
2013-05-26 19:25:03 +0300

    Add validation and fields
    Signed-off-by: Vik Ewoods <support@voroninstudio.eu>

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 class Article < ActiveRecord::Base
   attr_accessible :avatar, :full_descr, :name, :published, :short_descr, :slug
 
-  validates_presence_of :name, :slug, :short_descr
+  validates_presence_of :name, :slug, :short_descr, :avatar
   validates_uniqueness_of :name, :slug
   before_validation :get_url
 
@@ -17,6 +17,42 @@ class Article < ActiveRecord::Base
       :s3_credentials => Rails.root.join("config/amazon-s3.yml"),
       :path => '/:style/:id/:filename',
       :url  => ':s3_eu_url'
+
+  rails_admin do
+    list do
+      field :published do
+        label 'Опубликовано'
+      end
+      field :name do
+        label 'Название'
+      end
+      field :avatar do
+        label 'Изображение'
+      end
+    end
+
+    edit do
+      field :published do
+        label 'Опубликовано'
+      end
+      field :name do
+        label 'Название'
+      end
+      field :short_descr do
+        label 'Короткое описание'
+      end
+      field :full_descr do
+        label 'Полное описание'
+        ckeditor do
+          true
+        end
+      end
+      field :avatar do
+        label 'Изображение'
+      end
+      field :slug
+    end
+  end
 
   def to_param
     slug
